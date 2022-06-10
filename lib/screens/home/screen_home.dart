52d2bb36dac862e784f1e58448e83d5b11458a1f
GOKULKRISHNA V <macmo2004@gmail.com>
2022-06-10 18:50:55 +0530

    --Commit #2-- This is the second commit of the Personal Budget Project. Changes made to the files are as follows--: ************************************************* --Bottom Navigation Bar-- Created a new widget named BottomNavigatorWidget with an aim to facilitate switching between Transaction screen and Category screen. This widget is added in the HomeScreen. HomeScreen is still kept as a stateless widget and ValueListenableBuilder is used to switch the state of the UI. ************************************************* --ScreenTransaction and ScreenCategory-- Two new screens are created and the latter is given as the output of the Bottom Navigation Bar. ************************************************* The program was tested in an emulator and it worked as expected. No bugs were detected in the test run. THANK YOU...

--- lib/screens/home/screen_home.dart
+++ lib/screens/home/screen_home.dart
@@ -1,12 +1,29 @@
+import 'package:budget/screens/categories/screen_category.dart';
+import 'package:budget/screens/home/widgets/bottom_navigation_bar.dart';
+import 'package:budget/screens/transactions/screen_transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class ScreenHome extends StatelessWidget {
-  const ScreenHome({Key? key}) : super(key: key);
+  ScreenHome({Key? key}) : super(key: key);
+
+  static ValueNotifier<int> selectedindex = ValueNotifier(0);
+  final _pages = [
+    ScreenTransaction(),
+    ScreenCategory(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(bottomNavigationBar: const BottomNavigatorWidget(),
+      body: SafeArea(
+        child: ValueListenableBuilder(
+            valueListenable: selectedindex,
+            builder: (BuildContext context, int updatedindex, _) {
+              return _pages[updatedindex];
+            }),
+      ),
+    );
   }
 }
---
+++ lib/screens/home/widgets/bottom_navigation_bar.dart
@@ -0,0 +1,27 @@
+import 'package:budget/screens/home/screen_home.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavigatorWidget extends StatelessWidget {
+  const BottomNavigatorWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: ScreenHome.selectedindex,
+      builder: (BuildContext ctx, int updatedindex, Widget? _) {
+        return BottomNavigationBar(
+            unselectedItemColor: Colors.grey,
+            currentIndex: updatedindex,
+            onTap: (newIndex) {
+              ScreenHome.selectedindex.value = newIndex;
+            },
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.currency_exchange), label: 'Transactions'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.category), label: 'Categories')
+            ]);
+      },
+    );
+  }
+}
